2bdfd86555a77e77fc9f2ae61aab43e10e1ea046
Danny Baumann <dannybaumann@web.de>
2014-11-13 14:33:24 +0100

    Proper supplementary service notification handling (4/5).
    Move toast generation to correct place, and abstraction for pieces of
    data we want to map into InCallUI.
    
    Change-Id: Ia249c926d3228d850064a705454c42ec9a53c524

--- src/com/android/services/telephony/GsmConnection.java
+++ src/com/android/services/telephony/GsmConnection.java
@@ -16,14 +16,59 @@
 
 package com.android.services.telephony;
 
+import android.os.AsyncResult;
+import android.os.Handler;
+import android.os.Message;
+
 import com.android.internal.telephony.Connection;
+import com.android.internal.telephony.GsmCdmaConnection;
+import com.android.internal.telephony.Phone;
+import com.android.internal.telephony.gsm.SuppServiceNotification;
 
 /**
  * Manages a single phone call handled by GSM.
  */
 final class GsmConnection extends TelephonyConnection {
+    private static final int MSG_SUPP_SERVICE_NOTIFY = 1;
+
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SUPP_SERVICE_NOTIFY:
+                    Log.v(GsmConnection.this, "MSG_SUPP_SERVICE_NOTIFY");
+                    AsyncResult ar = (AsyncResult) msg.obj;
+                    SuppServiceNotification ssn = (SuppServiceNotification) ar.result;
+
+                    if (ssn != null) {
+                        onSuppServiceNotification(ssn);
+                    }
+                    break;
+            }
+        }
+    };
+
+    private boolean mIsForwarded;
+    private boolean mRemoteIncomingCallsBarred;
+
     GsmConnection(Connection connection, String telecomCallId, boolean isOutgoing) {
         super(connection, telecomCallId, isOutgoing);
+        updateConnectionProperties();
+    }
+
+    @Override
+    void setOriginalConnection(Connection originalConnection) {
+        super.setOriginalConnection(originalConnection);
+        getPhone().registerForSuppServiceNotification(mHandler, MSG_SUPP_SERVICE_NOTIFY, null);
+    }
+
+    @Override
+    void clearOriginalConnection() {
+        Phone phone = getPhone();
+        if (phone != null) {
+            phone.unregisterForSuppServiceNotification(mHandler);
+        }
+        super.clearOriginalConnection();
     }
 
     /**
@@ -64,6 +109,17 @@ final class GsmConnection extends TelephonyConnection {
         if ((getConnectionProperties() & PROPERTY_IS_DOWNGRADED_CONFERENCE) != 0) {
             properties |= PROPERTY_IS_DOWNGRADED_CONFERENCE;
         }
+
+        if (getOriginalConnection() instanceof GsmCdmaConnection) {
+            GsmCdmaConnection gc = (GsmCdmaConnection) getOriginalConnection();
+            if (gc.isForwarded()) {
+                properties |= PROPERTY_WAS_FORWARDED;
+            }
+        }
+        if (mRemoteIncomingCallsBarred) {
+            properties |= PROPERTY_REMOTE_INCOMING_CALLS_BARRED;
+        }
+
         return properties;
     }
 
@@ -93,5 +149,42 @@ final class GsmConnection extends TelephonyConnection {
     @Override
     void onRemovedFromCallService() {
         super.onRemovedFromCallService();
+    }
+
+    private void onSuppServiceNotification(SuppServiceNotification notification) {
+        Phone phone = getPhone();
+        int state = getState();
+
+        Log.d(this, "SS Notification: " + notification);
+
+        if (notification.history != null) {
+            Bundle extras = new Bundle();
+            Log.v(GsmConnection.this, "Updating call history info in extras.");
+            extras.putStringArrayList(Connection.EXTRA_LAST_FORWARDED_NUMBER,
+                    new ArrayList(Arrays.asList(notification.history)));
+            putExtras(lastForwardedNumber);
+        }
+
+        if (notification.notificationType == SuppServiceNotification.NOTIFICATION_TYPE_MT) {
+            if (notification.code == SuppServiceNotification.MT_CODE_CALL_ON_HOLD) {
+                sendConnectionEvent(EVENT_CALL_REMOTELY_HELD, null);
+            } else if (notification.code == SuppServiceNotification.MT_CODE_CALL_RETRIEVED) {
+                sendConnectionEvent(EVENT_CALL_REMOTELY_UNHELD, null);
+            } else if (notification.code ==
+                    SuppServiceNotification.MT_CODE_ADDITIONAL_CALL_FORWARDED) {
+                sendConnectionEvent(EVENT_ADDITIONAL_CALL_FORWARDED, null);
+            }
+        } else if (notification.notificationType == SuppServiceNotification.NOTIFICATION_TYPE_MO) {
+            if (notification.code == SuppServiceNotification.MO_CODE_CALL_IS_WAITING) {
+                if (state == STATE_DIALING) {
+                    sendConnectionEvent(EVENT_DIALING_IS_WAITING, null);
+                }
+            } else if (notification.code ==
+                    SuppServiceNotification.MO_CODE_INCOMING_CALLS_BARRED) {
+                mRemoteIncomingCallsBarred = true;
+            }
+        }
+
+        updateConnectionProperties();
     }
 }
