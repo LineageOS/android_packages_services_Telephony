9bbd51192cf4eb0050718b565eef0e70e2b29d55
dhacker29 <dhackerdvm@gmail.com>
2015-11-02 23:50:36 -0500

    CallFeatureSettings: Only show video quality if QTI package installed
    Shamu uses org.codeaurora.ims package and different constants for video
    quality settings, so this crashes on open.
    
    Change-Id: I701635d475a85c16556e716606ec59aad24e8948

--- Android.mk
+++ Android.mk
@@ -13,6 +13,7 @@ LOCAL_JAVA_LIBRARIES := telephony-common voip-common ims-common telephony-ext
 LOCAL_STATIC_JAVA_LIBRARIES := \
         ims-ext-common \
         org.apache.http.legacy \
+        org.cyanogenmod.platform.internal \
         guava \
         volley
 
