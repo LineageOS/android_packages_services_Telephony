8d29566f25fe461b0c82f86c84b49a63d8788c03
nullghost <justin.b.wood@gmail.com>
2017-05-05 10:05:29 -0400

    Phone: Sprint MWI Quirk: Phantom message wait indicator workaround
    Squashed
    https://review.lineageos.org/#/c/140883/   initial rebase
    https://review.lineageos.org/#/c/140838/   fix missing code
    https://review.lineageos.org/#/c/140830/   fix NPEs
    
    If true, message wait indicators are hidden by default on this device
    unless the user enables it in Call Settings. This quirk is necessary
    for the "Phantom Voicemail" issue where notifications that are
    impossible to dismiss on the Sprint network. In rare instances, feature
    phone voicemail gets stuck in a Sprint account where smart phones are
    unable to delete them. Sprint will not help the subscriber fix this,
    as their own Android phones ignore message wait indicators.
    
    Enable 'sprint_mwi_quirk' in overlay/packages/services/Telephony/res/values/config.xml
    for this option to appear in the Phone app.
    
    Phone > Settings > Calls > Voicemail > Message wait indicator
    
    Default is disabled, as Sprint Android phones have no reason to see
    message wait indicators. It can however be enabled for Sprint phones
    used on overseas carriers where message wait indicators are desired.
    
    Rebased on cm-14.1
    
    Additional changes already merged
    https://github.com/LineageOS/cm_platform_sdk/blob/cm-14.1/sdk/src/java/cyanogenmod/providers/CMSettings.java#L1263
    
    Change-Id: I257f165ec21bac78d41fc0dd810368139f1c8f2d

--- src/com/android/phone/settings/VoicemailSettingsActivity.java
+++ src/com/android/phone/settings/VoicemailSettingsActivity.java
@@ -57,6 +57,8 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 
+import cyanogenmod.providers.CMSettings;
+
 public class VoicemailSettingsActivity extends PreferenceActivity
         implements DialogInterface.OnClickListener,
                 Preference.OnPreferenceChangeListener,
@@ -107,6 +109,8 @@ public class VoicemailSettingsActivity extends PreferenceActivity
     private static final String BUTTON_VOICEMAIL_KEY = "button_voicemail_key";
     private static final String BUTTON_VOICEMAIL_PROVIDER_KEY = "button_voicemail_provider_key";
     private static final String BUTTON_VOICEMAIL_SETTING_KEY = "button_voicemail_setting_key";
+    private static final String BUTTON_VOICEMAIL_CATEGORY_KEY = "button_voicemail_category_key";
+    private static final String BUTTON_MWI_NOTIFICATION_KEY = "button_mwi_notification_key";
 
     /** Event for Async voicemail change call */
     private static final int EVENT_VOICEMAIL_CHANGED        = 500;
@@ -205,6 +209,7 @@ public class VoicemailSettingsActivity extends PreferenceActivity
     private EditPhoneNumberPreference mSubMenuVoicemailSettings;
     private VoicemailProviderListPreference mVoicemailProviders;
     private PreferenceScreen mVoicemailSettings;
+    private SwitchPreference mEnableMwiNotification;
     private VoicemailRingtonePreference mVoicemailNotificationRingtone;
     private CheckBoxPreference mVoicemailNotificationVibrate;
     private SwitchPreference mVoicemailVisualVoicemail;
@@ -254,6 +259,23 @@ public class VoicemailSettingsActivity extends PreferenceActivity
             mSubMenuVoicemailSettings.setParentActivity(this, VOICEMAIL_PREF_ID, this);
             mSubMenuVoicemailSettings.setDialogOnClosedListener(this);
             mSubMenuVoicemailSettings.setDialogTitle(R.string.voicemail_settings_number_label);
+        }
+
+        mEnableMwiNotification = (SwitchPreference) findPreference(BUTTON_MWI_NOTIFICATION_KEY);
+        if (mEnableMwiNotification != null) {
+            if (getResources().getBoolean(R.bool.sprint_mwi_quirk)) {
+                mEnableMwiNotification.setOnPreferenceChangeListener(this);
+                int mwiNotificationEnabled = CMSettings.System.getInt(getContentResolver(),
+                        CMSettings.System.ENABLE_MWI_NOTIFICATION, 0);
+                mEnableMwiNotification.setChecked(mwiNotificationEnabled != 0);
+            } else {
+                PreferenceScreen voicemailCategory =
+                        (PreferenceScreen) findPreference(BUTTON_VOICEMAIL_CATEGORY_KEY);
+                if (voicemailCategory != null) {
+                    voicemailCategory.removePreference(mEnableMwiNotification);
+                }
+                mEnableMwiNotification = null;
+            }
         }
 
         mVoicemailProviders = (VoicemailProviderListPreference) findPreference(
@@ -379,6 +401,11 @@ public class VoicemailSettingsActivity extends PreferenceActivity
                 mVMProviderSettingsForced = false;
                 return false;
             }
+        } else if (preference == mEnableMwiNotification) {
+            CMSettings.System.putInt(mPhone.getContext().getContentResolver(),
+                    CMSettings.System.ENABLE_MWI_NOTIFICATION,
+                    mEnableMwiNotification.isChecked() ? 1 : 0);
+            return true;
         }
         return false;
     }
@@ -421,13 +448,13 @@ public class VoicemailSettingsActivity extends PreferenceActivity
                 mChangingVMorFwdDueToProviderChange = true;
                 saveVoiceMailAndForwardingNumber(newProviderKey, newProviderSettings);
             }
-        } else if (preference.getKey().equals(mVoicemailNotificationVibrate.getKey())) {
+        } else if (mVoicemailNotificationVibrate != null && preference.getKey().equals(mVoicemailNotificationVibrate.getKey())) {
             // Check key instead of comparing reference because closing the voicemail notification
             // ringtone dialog invokes onResume(), but leaves the old preference screen up,
             // TODO: Revert to checking reference after migrating voicemail to its own activity.
             VoicemailNotificationSettingsUtil.setVibrationEnabled(
                     mPhone, Boolean.TRUE.equals(objValue));
-        } else if (preference.getKey().equals(mVoicemailVisualVoicemail.getKey())) {
+        } else if (mVoicemailVisualVoicemail != null && preference.getKey().equals(mVoicemailVisualVoicemail.getKey())) {
             boolean isEnabled = (boolean) objValue;
             VisualVoicemailSettingsUtil
                     .setEnabled(mPhone.getContext(), mPhoneAccountHandle, isEnabled);
