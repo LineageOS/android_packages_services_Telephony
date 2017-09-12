c778d6f895a3c08c9c535315693036be1dda281f
Malcolm Chen <refuhoo@google.com>
2017-09-12 13:15:46 -0700

    Fix incorrect type cast of Carrier preference.
    Bug: 65556113
    Test: Manual
    
    Change-Id: I8cbfc3c2dc6410a6bc2da5d3fa41bc14f12d6044
    Merged-In: I98f81489319f6cda6921b88eb04e79978cad800d

--- src/com/android/phone/MobileNetworkSettings.java
+++ src/com/android/phone/MobileNetworkSettings.java
@@ -1713,8 +1713,7 @@ public class MobileNetworkSettings extends Activity  {
             PreferenceCategory networkOperatorCategory =
                     (PreferenceCategory) prefSet.findPreference(
                             NetworkOperators.CATEGORY_NETWORK_OPERATORS_KEY);
-            PreferenceScreen carrierSettings =
-                    (PreferenceScreen) prefSet.findPreference(BUTTON_CARRIER_SETTINGS_KEY);
+            Preference carrierSettings = prefSet.findPreference(BUTTON_CARRIER_SETTINGS_KEY);
             if (apnExpand != null) {
                 apnExpand.setEnabled(isWorldMode() || enable);
             }
