2b3ae382271e5f85da7dc31a1245319d5585bb82
Brad Ebinger <breadley@google.com>
2018-01-23 13:45:58 -0800

    Modify ImsService API namespaces
    Modifies the namespaces of ImsService
    API related classes to suppor the
    move to @SystemApi.
    
    Bug: 63987047
    Test: Unit Tests
    Change-Id: I8f9423900f608eaa69d861406144e243f8f6c495

--- src/com/android/services/telephony/TelephonyConnection.java
+++ src/com/android/services/telephony/TelephonyConnection.java
@@ -35,10 +35,10 @@ import android.telecom.VideoProfile;
 import android.telephony.CarrierConfigManager;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.TelephonyManager;
+import android.telephony.ims.ImsCallProfile;
 import android.util.Pair;
 
 import com.android.ims.ImsCall;
-import com.android.ims.ImsCallProfile;
 import com.android.internal.telephony.Call;
 import com.android.internal.telephony.CallFailCause;
 import com.android.internal.telephony.CallStateException;
