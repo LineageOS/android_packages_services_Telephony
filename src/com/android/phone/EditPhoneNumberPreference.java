54c836d4f50f6d5857566fe424b058d834a99c2a
Asher Simonds <dayanhammer@gmail.com>
2018-02-12 10:41:41 +0100

    Telephony: Bring in the new icon
    * Part of the new iconset made by Asher Simonds
    
    Co-authored-by: Michael W <baddaemon87@gmail.com>
    Change-Id: I541cb7b408483063ef1ad73719e7dba826228169

--- src/com/android/phone/EditPhoneNumberPreference.java
+++ src/com/android/phone/EditPhoneNumberPreference.java
@@ -277,7 +277,7 @@ public class EditPhoneNumberPreference extends EditTextPreference {
             }
         }
         // set the call icon on the title.
-        builder.setIcon(R.mipmap.ic_launcher_phone);
+        builder.setIcon(R.mipmap.ic_launcher);
     }
 
 
