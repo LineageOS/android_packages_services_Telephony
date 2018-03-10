474602a4f0d99f509a991383f5fba2a134b3b854
Paul Keith <javelinanddart@gmail.com>
2018-03-10 05:16:20 +0100

    Telephony: Fallback gracefully for emergency calls if suitable app isn't found
    * We can safely fall back to Google's if the user-chosen app doesn't
      support emergency calls, and if Google's is present on the device
    * Show an alert dialog to the user if no suitable app is found
      for handling emergency calls instead of bringing down the system
    
    Change-Id: Ieda12656df7ddc0a9f7d4630f5c99a8596488908

--- src/com/android/phone/OutgoingCallBroadcaster.java
+++ src/com/android/phone/OutgoingCallBroadcaster.java
@@ -474,15 +474,28 @@ public class OutgoingCallBroadcaster extends Activity
                 // opposed to a Context which look up known android
                 // packages only)
                 final Resources resources = getResources();
-                invokeFrameworkDialer.setClassName(
-                        resources.getString(R.string.ui_default_package),
-                        resources.getString(R.string.dialer_default_class));
-                invokeFrameworkDialer.setAction(Intent.ACTION_DIAL);
-                invokeFrameworkDialer.setData(intent.getData());
-                if (DBG) Log.v(TAG, "onCreate(): calling startActivity for Dialer: "
-                               + invokeFrameworkDialer);
-                startActivity(invokeFrameworkDialer);
-                finish();
+                String[] packages = resources.getStringArray(R.array.trusted_dialer_packages);
+                String[] classes = resources.getStringArray(R.array.trusted_dialer_classes);
+                for (int i = 0; i < packages.length; i++) {
+                    try {
+                        invokeFrameworkDialer.setClassName(packages[i], classes[i]);
+                        invokeFrameworkDialer.setAction(Intent.ACTION_DIAL);
+                        invokeFrameworkDialer.setData(intent.getData());
+                        if (DBG) Log.v(TAG, "onCreate(): calling startActivity for Dialer: "
+                                       + invokeFrameworkDialer);
+                        startActivity(invokeFrameworkDialer);
+                        finish();
+                        return;
+                    } catch (IllegalArgumentException e) {
+                        continue;
+                    }
+                }
+
+                // No dialer was able to handle the emergency call
+                new AlertDialog.Builder(this)
+                        .setMessage(R.string.emergency_unsupported_dialer_message)
+                        .setPositiveButton(android.R.string.ok, null)
+                        .show();
                 return;
             }
             callNow = false;
