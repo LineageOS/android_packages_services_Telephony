d20fa96e08e86e66f456bb8f493eabe7c4612b9a
Hall Liu <hallliu@google.com>
2018-07-25 18:49:59 -0700

    Add plumbing for the 911 redial
    Provide a mechanism to swap out the original connection when the lower
    layers trigger a redial.
    
    Bug: 111235453
    Test: manual (modify IMS stack locally to always return the 380 error
    when dialing in airplane mode)
    
    Change-Id: Ie1cab49f040dd2b34b775bd9e5d7f97fbf7aafa9

--- src/com/android/services/telephony/TelephonyConnection.java
+++ src/com/android/services/telephony/TelephonyConnection.java
@@ -569,6 +569,12 @@ abstract class TelephonyConnection extends Connection {
             updateConnectionProperties();
             sendRttSessionRemotelyTerminated();
         }
+
+        @Override
+        public void onOriginalConnectionReplaced(
+                com.android.internal.telephony.Connection newConnection) {
+            setOriginalConnection(newConnection);
+        }
     };
 
     protected com.android.internal.telephony.Connection mOriginalConnection;
