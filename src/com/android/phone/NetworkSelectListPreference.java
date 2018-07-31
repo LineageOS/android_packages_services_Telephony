a326beb4154797c6f03c24fc3e3109beb145b055
LuK1337 <priv.luk@gmail.com>
2018-07-31 12:33:51 +0200

    Use proper summary for network select list preference on dsds/dsda/tsts
    Change-Id: I7e1484378b0005d50b0873f027865eee1c9cf014

--- src/com/android/phone/NetworkSelectListPreference.java
+++ src/com/android/phone/NetworkSelectListPreference.java
@@ -188,14 +188,15 @@ public class NetworkSelectListPreference extends ListPreference
         // This preference should share the same progressDialog with networkOperators category.
         mProgressDialog = progressDialog;
 
-        if (SubscriptionManager.isValidSubscriptionId(mSubId)) {
-            mPhoneId = SubscriptionManager.getPhoneId(mSubId);
-        }
-
         TelephonyManager telephonyManager = (TelephonyManager)
                 getContext().getSystemService(Context.TELEPHONY_SERVICE);
 
-        setSummary(telephonyManager.getNetworkOperatorName());
+        if (SubscriptionManager.isValidSubscriptionId(mSubId)) {
+            mPhoneId = SubscriptionManager.getPhoneId(mSubId);
+            setSummary(telephonyManager.getNetworkOperatorName(mSubId));
+        } else {
+            setSummary(telephonyManager.getNetworkOperatorName());
+        }
 
         setOnPreferenceChangeListener(this);
     }
