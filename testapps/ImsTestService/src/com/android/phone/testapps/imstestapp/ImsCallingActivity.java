4c46071ec9367780f34bff17a068af1181cb1cf2
Brad Ebinger <breadley@google.com>
2018-10-01 10:40:55 -0700

    Implement ImsManager API for Telephony and update testapps
    Test: ImsTestApp
    Merged-In: I0b766c549a74f192b676497b83bfb83a478cd54d
    Change-Id: I0b766c549a74f192b676497b83bfb83a478cd54d

--- testapps/ImsTestService/src/com/android/phone/testapps/imstestapp/ImsCallingActivity.java
+++ testapps/ImsTestService/src/com/android/phone/testapps/imstestapp/ImsCallingActivity.java
@@ -17,16 +17,102 @@
 package com.android.phone.testapps.imstestapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.telephony.SubscriptionManager;
+import android.telephony.ims.ImsMmTelManager;
 import android.telephony.ims.feature.MmTelFeature;
 import android.telephony.ims.stub.ImsRegistrationImplBase;
+import android.util.Log;
 import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class ImsCallingActivity extends Activity {
+
+    private static final String PREFIX_ITEM = "Capability Event: ";
+    private static final String PREFIX_VALUE = "Value: ";
+
+    private static class CapItem {
+        public String key;
+        public String value;
+
+        CapItem(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        CapItem(String key, int value) {
+            this.key = key;
+            this.value = String.valueOf(value);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            CapItem regItem = (CapItem) o;
+            return Objects.equals(key, regItem.key)
+                    && Objects.equals(value, regItem.value);
+        }
+
+        @Override
+        public int hashCode() {
+
+            return Objects.hash(key, value);
+        }
+    }
+
+    private static class CapItemAdapter extends ArrayAdapter<CapItem> {
+        CapItemAdapter(Context context, ArrayList<CapItem> regItems) {
+            super(context, 0, regItems);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            CapItem capItem = getItem(position);
+
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.config_item,
+                        parent, false);
+            }
+
+            TextView textItem = (TextView) convertView.findViewById(R.id.configItem);
+            TextView textValue = (TextView) convertView.findViewById(R.id.configValue);
+
+            textItem.setText(PREFIX_ITEM + capItem.key);
+            textValue.setText(PREFIX_VALUE + capItem.value);
+
+            return convertView;
+        }
+    }
+
+    private final ImsMmTelManager.CapabilityCallback mCapabilityCallback =
+            new ImsMmTelManager.CapabilityCallback() {
+
+        @Override
+        public void onCapabilitiesStatusChanged(
+                MmTelFeature.MmTelCapabilities capabilities) {
+            Log.i("ImsCallingActivity" , "onCapabilitiesStatusChanged:" + capabilities);
+            mCapabilityEvents.add(new CapItem("cap changed: ", capabilities.toString()));
+            notifyDataChanged();
+
+        }
+
+        private void notifyDataChanged() {
+            mCapabiltyEventAdapter.notifyDataSetChanged();
+        }
+    };
 
     //Capabilities available by service
     private CheckBox mCapVoiceAvailBox;
@@ -35,6 +121,12 @@ public class ImsCallingActivity extends Activity {
     private CheckBox mCapSmsAvailBox;
 
     private TextView mCapEnabledText;
+
+    private ArrayList<CapItem> mCapabilityEvents = new ArrayList<>();
+    private CapItemAdapter mCapabiltyEventAdapter;
+    private ListView mListView;
+
+    private ImsMmTelManager mImsManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +157,25 @@ public class ImsCallingActivity extends Activity {
     protected void onResume() {
         super.onResume();
         mmTelCapabilityChanged();
+
+        mCapabiltyEventAdapter = new CapItemAdapter(this, mCapabilityEvents);
+        mListView = (ListView) findViewById(R.id.cap_cb_list);
+        mListView.setAdapter(mCapabiltyEventAdapter);
+        try {
+            mImsManager = ImsMmTelManager.createForSubscriptionId(this,
+                    SubscriptionManager.getDefaultVoiceSubscriptionId());
+            Log.i("ImsCallingActivity", "onResume");
+            mImsManager.addMmTelCapabilityCallback(getMainExecutor(), mCapabilityCallback);
+        } catch (IllegalArgumentException e) {
+            Log.w("ImsCallingActivity", "illegal subscription ID.");
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mImsManager.removeMmTelCapabilityCallback(mCapabilityCallback);
+        mImsManager = null;
     }
 
     private void mmTelCapabilityChanged() {
