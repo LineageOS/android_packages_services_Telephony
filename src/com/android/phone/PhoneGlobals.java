dc09cd3b8aa3d26c38f8e1ea137ebb1d25465b5a
dianlujitao <dianlujitao@lineageos.org>
2018-11-14 19:45:39 +0800

    Don't start SIP service before decrypted
     * WiFi service is not considered as a core service so it won't start in
       min framework, but SipService accesses to WiFi service's API. This
       would lead to a fatal exception and trigger RescueParty.
     * We don't really need SipService during such early stage, so simply
       disable it to avoid invoking WifiManager.
    
    Change-Id: Ie471523a50f83519187c3be02b556aee4b679b4d

--- src/com/android/phone/PhoneGlobals.java
+++ src/com/android/phone/PhoneGlobals.java
@@ -248,7 +248,8 @@ public class PhoneGlobals extends ContextWrapper {
                     // This is the same procedure that is triggered in the SipIncomingCallReceiver
                     // upon BOOT_COMPLETED.
                     UserManager userManager = UserManager.get(sMe);
-                    if (userManager != null && userManager.isUserUnlocked()) {
+                    if (userManager != null && userManager.isUserUnlocked()
+                            && !isDataEncrypted()) {
                         SipUtil.startSipService();
                     }
                     break;
@@ -685,6 +686,11 @@ public class PhoneGlobals extends ContextWrapper {
         }
     }
 
+    private static boolean isDataEncrypted() {
+        String voldState = SystemProperties.get("vold.decrypt");
+        return "1".equals(voldState) || "trigger_restart_min_framework".equals(voldState);
+    }
+
     /**
      * Receiver for misc intent broadcasts the Phone app cares about.
      */
@@ -762,7 +768,7 @@ public class PhoneGlobals extends ContextWrapper {
             String action = intent.getAction();
 
             SipAccountRegistry sipAccountRegistry = SipAccountRegistry.getInstance();
-            if (action.equals(Intent.ACTION_BOOT_COMPLETED)) {
+            if (action.equals(Intent.ACTION_BOOT_COMPLETED) && !isDataEncrypted()) {
                 SipUtil.startSipService();
             } else if (action.equals(SipManager.ACTION_SIP_SERVICE_UP)
                     || action.equals(SipManager.ACTION_SIP_CALL_OPTION_CHANGED)) {
