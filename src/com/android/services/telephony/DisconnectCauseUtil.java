150ce2e3537dd7920207df02a7502d6a53c2f6b5
Tyler Gunn <tgunn@google.com>
2019-03-25 09:58:04 -0700

    Provide error message for IMS access blocked error code.
    This will remap to the human-readable string:
    Network is busy.  Please try your call again later.
    
    Test: Compile
    Fixes: 122834587
    Change-Id: Ib42eba5392bb7ff1bb3966bf6e3772cfac74bbc4

--- src/com/android/services/telephony/DisconnectCauseUtil.java
+++ src/com/android/services/telephony/DisconnectCauseUtil.java
@@ -253,6 +253,10 @@ public class DisconnectCauseUtil {
                 resourceId = R.string.callFailed_NetworkBusy;
                 break;
 
+            case android.telephony.DisconnectCause.IMS_ACCESS_BLOCKED:
+                resourceId = R.string.callFailed_NetworkCongested;
+                break;
+
             case android.telephony.DisconnectCause.CONGESTION:
                 resourceId = R.string.callFailed_congestion;
                 break;
@@ -563,6 +567,10 @@ public class DisconnectCauseUtil {
 
             case android.telephony.DisconnectCause.CDMA_REORDER:
                 resourceId = R.string.callFailed_NetworkBusy;
+                break;
+
+            case android.telephony.DisconnectCause.IMS_ACCESS_BLOCKED:
+                resourceId = R.string.callFailed_NetworkCongested;
                 break;
 
             case android.telephony.DisconnectCause.FDN_BLOCKED:
