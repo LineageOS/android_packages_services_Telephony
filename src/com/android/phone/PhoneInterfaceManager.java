f18a0cf062afaf8611173d4e1faa47cf60823073
Hall Liu <hallliu@google.com>
2019-04-17 13:37:11 -0700

    Change ServiceState and TelephonyRegistry logging
    When an app bypasses a location access check due to its target SDK for
    queries to ServiceState or when we're pushing out info through
    TelephonyRegistry, log it as info instead of error to avoid spamming the
    logs too much.
    
    Fixes: 130668054
    Test: manual
    Change-Id: Ic77f45cf0cd70432836126c9e9d3c2156456ae7e

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -5543,6 +5543,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
                                 .setCallingPid(Binder.getCallingPid())
                                 .setCallingUid(Binder.getCallingUid())
                                 .setMethod("getServiceStateForSubscriber")
+                                .setLogAsInfo(true)
                                 .setMinSdkVersionForFine(Build.VERSION_CODES.Q)
                                 .build());
 
@@ -5553,6 +5554,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
                                 .setCallingPid(Binder.getCallingPid())
                                 .setCallingUid(Binder.getCallingUid())
                                 .setMethod("getServiceStateForSubscriber")
+                                .setLogAsInfo(true)
                                 .setMinSdkVersionForCoarse(Build.VERSION_CODES.Q)
                                 .build());
         // We don't care about hard or soft here -- all we need to know is how much info to scrub.
