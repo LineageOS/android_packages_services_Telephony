7bc8e0730eab26cbdd750319475308c3320471aa
chen xu <fionaxu@google.com>
2019-05-28 15:20:57 -0700

    skip permission check for TelephonyManager API getMmsUserAgent
    Bug: 133637995
    Test: Manual test
    Change-Id: I4d41e96de6662e9d16825554192114d74afb8551
    (cherry picked from commit d5ca2d5418cd4e4cdbc268f8b8579e80cd89efce)
    Merged-in: I4d41e96de6662e9d16825554192114d74afb8551

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -7035,4 +7035,28 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
             Binder.restoreCallingIdentity(identity);
         }
     }
+
+    @Override
+    public String getMmsUAProfUrl(int subId) {
+        //TODO investigate if this API should require proper permission check in R b/133791609
+        final long identity = Binder.clearCallingIdentity();
+        try {
+            return SubscriptionManager.getResourcesForSubId(getDefaultPhone().getContext(), subId)
+                    .getString(com.android.internal.R.string.config_mms_user_agent_profile_url);
+        } finally {
+            Binder.restoreCallingIdentity(identity);
+        }
+    }
+
+    @Override
+    public String getMmsUserAgent(int subId) {
+        //TODO investigate if this API should require proper permission check in R b/133791609
+        final long identity = Binder.clearCallingIdentity();
+        try {
+            return SubscriptionManager.getResourcesForSubId(getDefaultPhone().getContext(), subId)
+                    .getString(com.android.internal.R.string.config_mms_user_agent);
+        } finally {
+            Binder.restoreCallingIdentity(identity);
+        }
+    }
 }
