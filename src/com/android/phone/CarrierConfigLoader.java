f9a5b5f009bf99727f93cb6c288767baaf6db4e9
maxwen <max.weninger@gmail.com>
2019-11-18 23:45:39 +0100

    Telephony: use Build.VERSION.INCREMENTAL to check for system update
    Change-Id: Idcd7ad36150c1027c4b59a71bef852edfd74d45d

--- src/com/android/phone/CarrierConfigLoader.java
+++ src/com/android/phone/CarrierConfigLoader.java
@@ -538,16 +538,16 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
                     SharedPreferences sharedPrefs =
                             PreferenceManager.getDefaultSharedPreferences(mContext);
                     final String lastFingerprint = sharedPrefs.getString(KEY_FINGERPRINT, null);
-                    if (!Build.FINGERPRINT.equals(lastFingerprint)) {
+                    if (!Build.VERSION.INCREMENTAL.equals(lastFingerprint)) {
                         logd(
-                                "Build fingerprint changed. old: "
+                                "Build incremental version changed. old: "
                                         + lastFingerprint
                                         + " new: "
-                                        + Build.FINGERPRINT);
+                                        + Build.VERSION.INCREMENTAL);
                         clearCachedConfigForPackage(null);
                         sharedPrefs
                                 .edit()
-                                .putString(KEY_FINGERPRINT, Build.FINGERPRINT)
+                                .putString(KEY_FINGERPRINT, Build.VERSION.INCREMENTAL)
                                 .apply();
                     }
                     break;
