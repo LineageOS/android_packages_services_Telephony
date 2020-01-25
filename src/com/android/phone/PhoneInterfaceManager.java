1d9045cbca8a3c1bd2aa9d6482815b8f32abbd38
Hall Liu <hallliu@google.com>
2020-01-24 18:07:12 -0800

    Enforce ACCESS_COARSE_LOCATION on getCellLocation
    A implementation bug in Q meant that we weren't checking
    ACCESS_COARSE_LOCATION on apps targeting API 28 or earlier on
    getCellLocation. This change patches this issue.
    
    Bug: 147402629
    Bug: 148414207
    Test: atest TelephonyManagerTest:#testSdk28CellLocation
    Change-Id: Ie1f622307dec66500b83e960a44f49c37068007e
    (cherry picked from commit 773ba0257913f3fa3fc1e28d33826ea6cbdf86cd)
    (cherry picked from commit c3f8eb66822b3f32d3af7b78d2220d9a53d8091d)

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -1913,6 +1913,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
                                 .setCallingPid(Binder.getCallingPid())
                                 .setCallingUid(Binder.getCallingUid())
                                 .setMethod("getCellLocation")
+                                .setMinSdkVersionForCoarse(Build.VERSION_CODES.BASE)
                                 .setMinSdkVersionForFine(Build.VERSION_CODES.Q)
                                 .build());
         switch (locationResult) {
