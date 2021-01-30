0bd2599b8e016f97b6c38b5f6e287532e89a7fc7
Tyler Gunn <tgunn@google.com>
2021-01-29 16:28:36 -0800

    Hook in DTMF Transport.
    Add the DtmfTransport to the Communicator created in TelephonyConnection.
    
    Test: Added unit tests for DTMF transport.
    Test: Manual test to verify protocol is attempted when call goes active.
    Bug: 163085177Bug:
    Change-Id: Id967b8f6aa71e8ea362772be543207325782e8c4

--- src/com/android/services/telephony/TelephonyConnection.java
+++ src/com/android/services/telephony/TelephonyConnection.java
@@ -18,6 +18,7 @@ package com.android.services.telephony;
 
 import android.annotation.NonNull;
 import android.annotation.Nullable;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.drawable.Icon;
@@ -64,8 +65,11 @@ import com.android.internal.telephony.Connection.PostDialListener;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneConstants;
 import com.android.internal.telephony.d2d.Communicator;
+import com.android.internal.telephony.d2d.DtmfAdapter;
+import com.android.internal.telephony.d2d.DtmfTransport;
 import com.android.internal.telephony.d2d.RtpAdapter;
 import com.android.internal.telephony.d2d.RtpTransport;
+import com.android.internal.telephony.d2d.Timeouts;
 import com.android.internal.telephony.gsm.SuppServiceNotification;
 import com.android.internal.telephony.imsphone.ImsPhone;
 import com.android.internal.telephony.imsphone.ImsPhoneCall;
@@ -86,6 +90,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
 
 /**
  * Base class for CDMA and GSM connections.
@@ -842,6 +847,8 @@ abstract class TelephonyConnection extends Connection implements Holdable, Commu
     private D2DCallStateAdapter mD2DCallStateAdapter;
 
     private RtpTransport mRtpTransport;
+
+    private DtmfTransport mDtmfTransport;
 
     /**
      * Facilitates device to device communication.
@@ -3202,7 +3209,20 @@ abstract class TelephonyConnection extends Connection implements Holdable, Commu
             }
         };
         mRtpTransport = new RtpTransport(rtpAdapter, null /* TODO: not needed yet */, mHandler);
-        mCommunicator = new Communicator(List.of(mRtpTransport), this);
+
+        DtmfAdapter dtmfAdapter = digit -> {
+            if (!isImsConnection()) {
+                Log.w(TelephonyConnection.this, "sendDtmf: not an ims conn.");
+            }
+            Log.d(TelephonyConnection.this, "sendDtmf: send digit %c", digit);
+            ImsPhoneConnection originalConnection =
+                    (ImsPhoneConnection) mOriginalConnection;
+            originalConnection.getImsCall().sendDtmf(digit, null /* result msg not needed */);
+        };
+        ContentResolver cr = getPhone().getContext().getContentResolver();
+        mDtmfTransport = new DtmfTransport(dtmfAdapter, new Timeouts.Adapter(cr),
+                Executors.newSingleThreadScheduledExecutor());
+        mCommunicator = new Communicator(List.of(mRtpTransport, mDtmfTransport), this);
         mD2DCallStateAdapter = new D2DCallStateAdapter(mCommunicator);
         addTelephonyConnectionListener(mD2DCallStateAdapter);
     }
