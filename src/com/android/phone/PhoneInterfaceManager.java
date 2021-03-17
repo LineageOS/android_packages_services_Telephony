4ac4f987c4c52d61167f0e7c0b970fff99eff1a7
Alireza Forouzan <alirezaf@google.com>
2021-03-16 22:18:52 -0700

    Check CarrierPriviledge for getPhoneAccountHandleForSubscriptionId
    Currently getPhoneAccountHandleForSubscriptionId requires
    READ_PRIVILEGED_PHONE_STATE permission. This change allows carrier
    priviledged app to get the phone account handle of its own
    subcriptionId.
    
    Bug: 182966851
    Test: manual
    Change-Id: If23542fb003eb8af6299ffe6945c331b09ee9d9e

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -7351,8 +7351,11 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
 
     @Override
     public @Nullable PhoneAccountHandle getPhoneAccountHandleForSubscriptionId(int subscriptionId) {
-        enforceReadPrivilegedPermission("getPhoneAccountHandleForSubscriptionId, "
-                + "subscriptionId: " + subscriptionId);
+        TelephonyPermissions
+                .enforeceCallingOrSelfReadPrivilegedPhoneStatePermissionOrCarrierPrivilege(
+                mApp,
+                subscriptionId,
+                "getPhoneAccountHandleForSubscriptionId, " + "subscriptionId: " + subscriptionId);
         final long identity = Binder.clearCallingIdentity();
         try {
             Phone phone = getPhone(subscriptionId);
