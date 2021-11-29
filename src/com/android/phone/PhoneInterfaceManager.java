2c4509528d4511e9fd76104e376b664b08f20784
Jack Yu <jackyu@google.com>
2021-11-29 11:36:17 -0800

    Added several changes in data stack
    1. Prepared for removing the old data stack.
    2. Fixed minor bugs.
    3. Enhanced DataNetworkController callback.
    4. Supported overall internet state.
    
    Bug: 196597630
    Test: Manual
    Change-Id: I99a833caac2f6f47a1fb6d050900c8fb2db3a8e3

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -2939,6 +2939,9 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
         try {
             final Phone phone = getPhone(subId);
             if (phone != null) {
+                if (phone.isUsingNewDataStack()) {
+                    return phone.getDataNetworkController().getInternetDataNetworkState();
+                }
                 return PhoneConstantConversions.convertDataState(phone.getDataConnectionState());
             } else {
                 return PhoneConstantConversions.convertDataState(
