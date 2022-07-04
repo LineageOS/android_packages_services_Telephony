fe72cbf117e5f0fa862541527c2cfe61d6b77e28
ReallySnow <reallysnow233@gmail.com>
2022-07-04 21:48:32 +0800

    Telephony: Adapt to S style
    Change-Id: I3c1f4a2b8107e07b90cb60a413703e5884d49d61

--- src/com/android/phone/settings/RadioInfo.java
+++ src/com/android/phone/settings/RadioInfo.java
@@ -98,13 +98,13 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AlertDialog.Builder;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneFactory;
 import com.android.internal.telephony.euicc.EuiccConnector;
 import com.android.internal.telephony.util.TelephonyUtils;
 import com.android.phone.R;
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -123,7 +123,7 @@ import java.util.concurrent.TimeoutException;
  * Allows user to read and alter some of the radio related information.
  *
  */
-public class RadioInfo extends AppCompatActivity {
+public class RadioInfo extends CollapsingToolbarBaseActivity {
     private static final String TAG = "RadioInfo";
 
     private static final boolean IS_USER_BUILD = "user".equals(Build.TYPE);
