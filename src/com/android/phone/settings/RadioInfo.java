36d7e5f94dd097add2011e25bb07677cf631366c
Ling Ma <linggm@google.com>
2024-06-03 17:40:14 -0700

    Add esos questionnaire button
    The button triggers Questionnaire mode, invoked in real emergency situations for showing the E-SOS questionnaire to user and sending message over satellite.
    
    Fix: 343541805
    Test: tester verified
    Change-Id: Idd1e1205626a51b6d5b473f9473464047af9bc36
    Merged-In: Idd1e1205626a51b6d5b473f9473464047af9bc36
    (cherry picked from commit dfc5bca87ead3081af3391fe7ab238343cfe05d3)

--- src/com/android/phone/settings/RadioInfo.java
+++ src/com/android/phone/settings/RadioInfo.java
@@ -104,6 +104,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneFactory;
 import com.android.internal.telephony.euicc.EuiccConnector;
+import com.android.internal.telephony.flags.Flags;
 import com.android.internal.telephony.util.TelephonyUtils;
 import com.android.phone.R;
 
@@ -128,6 +129,9 @@ public class RadioInfo extends AppCompatActivity {
     private static final String TAG = "RadioInfo";
 
     private static final boolean IS_USER_BUILD = "user".equals(Build.TYPE);
+
+    private static final String ACTION_ESOS_TEST =
+            "com.google.android.apps.stargate.ACTION_ESOS_QUESTIONNAIRE";
 
     private static final String[] PREFERRED_NETWORK_LABELS = {
             "GSM/WCDMA preferred",
@@ -274,6 +278,7 @@ public class RadioInfo extends AppCompatActivity {
     private Button mOemInfoButton;
     private Button mCarrierProvisioningButton;
     private Button mTriggerCarrierProvisioningButton;
+    private Button mEsosButton;
     private Switch mImsVolteProvisionedSwitch;
     private Switch mImsVtProvisionedSwitch;
     private Switch mImsWfcProvisionedSwitch;
@@ -670,6 +675,17 @@ public class RadioInfo extends AppCompatActivity {
                     mTriggerCarrierProvisioningButtonHandler);
         } else {
             mTriggerCarrierProvisioningButton.setEnabled(false);
+        }
+
+        mEsosButton = (Button) findViewById(R.id.esos_questionnaire);
+        if (!Flags.oemEnabledSatelliteFlag()) {
+            mEsosButton.setVisibility(View.GONE);
+        } else {
+            mEsosButton.setOnClickListener(v ->
+                    mPhone.getContext().startActivity(
+                        new Intent(ACTION_ESOS_TEST)
+                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK))
+            );
         }
 
         mOemInfoButton = (Button) findViewById(R.id.oem_info);
