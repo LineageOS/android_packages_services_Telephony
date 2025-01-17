92f47baf4505e474376c0550e8b8cd25d19e74e2
Tyler Gunn <tgunn@google.com>
2025-01-17 23:34:46 +0000

    Prevent overlays on Telephony settings screen.
    Ensure only system overlays are allowed on telephony settings screens.
    
    Flag: NONE Security fix
    Bug: 335387175
    Test: Manually launched activities to verify no overlays are possible.
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:46e18ac57da3ac50271fc2be5b20bd7799637eae)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:a3cd8d0e73f5366eacb6d23462327b552187b2d0)
    Merged-In: I953667309753ff6fc966956e246512de97ad8638
    Change-Id: I953667309753ff6fc966956e246512de97ad8638

--- src/com/android/phone/CdmaCallForwardOptions.java
+++ src/com/android/phone/CdmaCallForwardOptions.java
@@ -73,6 +73,10 @@ public class CdmaCallForwardOptions extends TimeConsumingPreferenceActivity {
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
+        getWindow().addSystemFlags(
+                android.view.WindowManager.LayoutParams
+                        .SYSTEM_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS);
+
         addPreferencesFromResource(R.xml.callforward_options);
 
         mSubscriptionInfoHelper = new SubscriptionInfoHelper(this, getIntent());
