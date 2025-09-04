4c520a6bcc50c484cdb0555f71fe3d97c9174f12
Akash Garg <gargakash@google.com>
2025-09-04 13:36:57 +0000

    Restricting UserBuild from presistent carrierConfig Override
    Bug: 441823943
    Flag: EXEMPT Bugfix
    Test: manually verified
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:e129f3e192cc2082f5ea46676be6c882b2fe742e
    Merged-In: I915b1ffa30418bdb5df553057680e44b1b18af06
    Change-Id: I915b1ffa30418bdb5df553057680e44b1b18af06

--- src/com/android/phone/CarrierConfigLoader.java
+++ src/com/android/phone/CarrierConfigLoader.java
@@ -29,6 +29,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Binder;
@@ -1215,6 +1216,11 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
         overrideConfig(mOverrideConfigs, phoneId, overrides);
 
         if (persistent) {
+            if (isUserBuild() && !isSystemApp()) {
+                throw new SecurityException("overrideConfig with persistent=true only can be "
+                        + "invoked by system app");
+            }
+
             overrideConfig(mPersistentOverrideConfigs, phoneId, overrides);
 
             if (overrides != null) {
@@ -1231,6 +1237,25 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
             }
         }
         notifySubscriptionInfoUpdater(phoneId);
+    }
+
+    private boolean isSystemApp() {
+        try {
+            String callingPackage = mContext.getPackageManager().getNameForUid(
+                    Binder.getCallingUid());
+
+            ApplicationInfo appInfo = mContext.getPackageManager().getApplicationInfo(
+                    callingPackage, 0);
+            return (appInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0
+                    || (appInfo.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0;
+        } catch (Exception e) {
+            loge("isSystemApp: failed to get application info: " + e);
+            return false;
+        }
+    }
+
+    private boolean isUserBuild() {
+        return "user".equals(android.os.Build.TYPE);
     }
 
     private void overrideConfig(@NonNull PersistableBundle[] currentOverrides, int phoneId,
