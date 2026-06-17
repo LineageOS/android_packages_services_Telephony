4fe0d7081464f0d3eddf97f65ef11f63cf090dbe
Aishwarya Mallampati <amallampati@google.com>
2026-06-17 15:16:44 -0700

    Fix StackOverflowError in SatelliteEntitlementController
    Bug: 517377772
    Test: SatelliteEntitlementApiTest
    Flag: EXEMPT BUGFIX
    
    (cherry picked from commit 1f8bcf87291750ffeab00af17b9b82e8333f62b6)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:f1f3c63630a3203657f89169d6d71894f38c4b43
    Merged-In: I2a7f435026d66424bbab400a2d93261f5f8d1577
    Change-Id: I2a7f435026d66424bbab400a2d93261f5f8d1577

--- src/com/android/phone/satellite/entitlement/SatelliteEntitlementApi.java
+++ src/com/android/phone/satellite/entitlement/SatelliteEntitlementApi.java
@@ -58,11 +58,18 @@ public class SatelliteEntitlementApi {
 
         String response = mServiceEntitlement.queryEntitlementStatus(
                 ServiceEntitlement.APP_SATELLITE_ENTITLEMENT, request);
-        SatelliteEntitlementResponse satelliteEntitlementResponse =
-                new SatelliteEntitlementResponse(response);
-        return new SatelliteEntitlementResult(satelliteEntitlementResponse.getEntitlementStatus(),
-                satelliteEntitlementResponse.getPlmnAllowed(),
-                satelliteEntitlementResponse.getPlmnBarredList());
+        try {
+            SatelliteEntitlementResponse satelliteEntitlementResponse =
+                    new SatelliteEntitlementResponse(response);
+            return new SatelliteEntitlementResult(
+                    satelliteEntitlementResponse.getEntitlementStatus(),
+                    satelliteEntitlementResponse.getPlmnAllowed(),
+                    satelliteEntitlementResponse.getPlmnBarredList());
+        } catch (Throwable t) {
+            throw new ServiceEntitlementException(
+                    ServiceEntitlementException.ERROR_MALFORMED_HTTP_RESPONSE,
+                    "Failed to parse entitlement response", t);
+        }
     }
 
     @NonNull
